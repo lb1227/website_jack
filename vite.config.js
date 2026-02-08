318abdc8b57e6a514037a0fd2457f7c8113fde95
Luke <117487478+lb1227@users.noreply.github.com>
2026-02-08 18:51:30 -0500

    Add CSP meta for script eval

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/website_jack/",
+  base: "./",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
